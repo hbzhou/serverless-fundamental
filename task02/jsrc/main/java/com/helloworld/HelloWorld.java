6ccf94d7e40e8782ace8f615216a8d35d42f25ca
Jeremy Zhou <jeremygilbert@126.com>
2024-04-24 22:59:37 +0800

    task02

--- task02/jsrc/main/java/com/helloworld/HelloWorld.java
+++ task02/jsrc/main/java/com/helloworld/HelloWorld.java
@@ -3,7 +3,10 @@ package com.helloworld;
 import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.lambda.runtime.RequestHandler;
 import com.syndicate.deployment.annotations.lambda.LambdaHandler;
+import com.syndicate.deployment.annotations.lambda.LambdaUrlConfig;
 import com.syndicate.deployment.model.RetentionSetting;
+import com.syndicate.deployment.model.lambda.url.AuthType;
+import com.syndicate.deployment.model.lambda.url.InvokeMode;
 
 import java.util.HashMap;
 import java.util.Map;
