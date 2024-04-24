9f3a02d175bed6bc2babcdc2dc3d566a2006bd7a
Jeremy Zhou <jeremygilbert@126.com>
2024-04-24 22:53:45 +0800

    task02

---
+++ task02/jsrc/main/java/com/helloworld/HelloWorld.java
@@ -0,0 +1,32 @@
+package com.helloworld;
+
+import com.amazonaws.services.lambda.runtime.Context;
+import com.amazonaws.services.lambda.runtime.RequestHandler;
+import com.syndicate.deployment.annotations.lambda.LambdaHandler;
+import com.syndicate.deployment.model.RetentionSetting;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@LambdaHandler(lambdaName = "hello_world",
+		roleName = "hello_world-role",
+		logsExpiration = RetentionSetting.SYNDICATE_ALIASES_SPECIFIED
+)
+@LambdaUrlConfig(
+		authType = AuthType.NONE,
+		invokeMode = InvokeMode.BUFFERED
+)
+public class HelloWorld implements RequestHandler<Map<String, Object>, Map<String, Object>> {
+
+	public Map<String, Object> handleRequest(Map<String, Object> request, Context context) {
+		if (request.get("rawPath").equals("/hello")){
+			System.out.println("Hello from lambda");
+			Map<String, Object> resultMap = new HashMap<>();
+			Map<String, Object> body = Map.of("statusCode", 200, "message", "Hello from Lambda");
+			resultMap.put("body", body);
+			resultMap.put("statusCode", 200);
+			return resultMap;
+		}
+		return Map.of("statusCode", 404);
+	}
+}
