aa27e6f38eb47aa18fb5e0eb4606499b086fb5a9
Jeremy Zhou <jeremygilbert@126.com>
2024-04-25 20:46:36 +0800

    use object mapper to conver the boday

--- task02/jsrc/main/java/com/helloworld/HelloWorld.java
+++ task02/jsrc/main/java/com/helloworld/HelloWorld.java
@@ -2,6 +2,8 @@ package com.helloworld;
 
 import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.lambda.runtime.RequestHandler;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.syndicate.deployment.annotations.lambda.LambdaHandler;
 import com.syndicate.deployment.annotations.lambda.LambdaUrlConfig;
 import com.syndicate.deployment.model.RetentionSetting;
@@ -26,7 +28,11 @@ public class HelloWorld implements RequestHandler<Map<String, Object>, Map<Strin
 			System.out.println("Hello from lambda");
 			Map<String, Object> resultMap = new HashMap<>();
 			Map<String, Object> body = Map.of("statusCode", 200, "message", "Hello from Lambda");
-			resultMap.put("body", body);
+			try {
+				resultMap.put("body", new ObjectMapper().writeValueAsString(body));
+			} catch (JsonProcessingException e) {
+				throw new RuntimeException(e);
+			}
 			resultMap.put("statusCode", 200);
 			return resultMap;
 		}
