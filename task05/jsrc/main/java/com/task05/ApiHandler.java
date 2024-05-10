b197c381822b5b9efdaa00d5f1273a496fa98cf5
Jeremy Zhou <jeremygilbert@126.com>
2024-05-10 23:15:46 +0800

    task05

--- task05/jsrc/main/java/com/task05/ApiHandler.java
+++ task05/jsrc/main/java/com/task05/ApiHandler.java
@@ -1,46 +1,49 @@
 package com.task05;
 
+import com.amazonaws.services.dynamodbv2.AmazonDynamoDB;
 import com.amazonaws.services.dynamodbv2.AmazonDynamoDBClientBuilder;
-import com.amazonaws.services.dynamodbv2.model.AttributeValue;
-import com.amazonaws.services.dynamodbv2.model.PutItemRequest;
-import com.amazonaws.services.dynamodbv2.model.PutItemResult;
+import com.amazonaws.services.dynamodbv2.document.DynamoDB;
+import com.amazonaws.services.dynamodbv2.document.Item;
+import com.amazonaws.services.dynamodbv2.document.PutItemOutcome;
 import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.lambda.runtime.RequestHandler;
-import com.amazonaws.services.lambda.runtime.events.APIGatewayV2ProxyRequestEvent;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.syndicate.deployment.annotations.lambda.LambdaHandler;
 import com.syndicate.deployment.model.RetentionSetting;
 
 import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 import java.util.Map;
 import java.util.UUID;
 
 @LambdaHandler(lambdaName = "api_handler",
-	roleName = "api_handler-role",
-	logsExpiration = RetentionSetting.SYNDICATE_ALIASES_SPECIFIED
+        roleName = "api_handler-role",
+        logsExpiration = RetentionSetting.SYNDICATE_ALIASES_SPECIFIED
 )
-public class ApiHandler implements RequestHandler<APIGatewayV2ProxyRequestEvent, Map<String, Object>> {
-	private static final String TABLE_NAME = "Events";
-	private final ObjectMapper objectMapper = new ObjectMapper();
+public class ApiHandler implements RequestHandler<Map<String, Object>, Map<String, Object>> {
+    private static final String TABLE_NAME = "cmtr-8517cab4-Events-test";
+    private static final String REGION = "eu-central-1";
+    private final ObjectMapper objectMapper = new ObjectMapper();
 
-	public Map<String, Object> handleRequest(APIGatewayV2ProxyRequestEvent request, Context context) {
-		String body = request.getBody();
-		try {
-			ApiRequestBody apiRequestBody = objectMapper.readValue(body, ApiRequestBody.class);
-			PutItemRequest putItemRequest = new PutItemRequest();
-			putItemRequest.setTableName(TABLE_NAME);
-			putItemRequest.setItem(Map.of(
-					"id", new AttributeValue(UUID.randomUUID().toString()),
-					"principalId", new AttributeValue(String.valueOf(apiRequestBody.getPrincipalId())),
-					"content",  new AttributeValue(objectMapper.writeValueAsString(apiRequestBody.getContent())),
-					"createdAt", new AttributeValue(String.valueOf(LocalDateTime.now()))
-					)
-			);
-			PutItemResult putItemResult = AmazonDynamoDBClientBuilder.defaultClient().putItem(putItemRequest);
-			return Map.of("statusCode", 201, "event", putItemResult.getAttributes());
-		} catch (JsonProcessingException e) {
-			throw new RuntimeException(e);
-		}
-	}
+    public Map<String, Object> handleRequest(Map<String, Object> request, Context context) {
+        try {
+            ApiRequestBody apiRequestBody = objectMapper.readValue(objectMapper.writeValueAsString(request), ApiRequestBody.class);
+            AmazonDynamoDB client = AmazonDynamoDBClientBuilder.standard()
+                    .withRegion(REGION)
+                    .build();
+            DynamoDB dynamoDB = new DynamoDB(client);
+
+            Item item = new Item()
+                    .withPrimaryKey("id", UUID.randomUUID().toString())
+                    .withNumber("principalId", apiRequestBody.getPrincipalId())
+                    .withString("createdAt", LocalDateTime.now().format(DateTimeFormatter.ISO_DATE_TIME))
+                    .withMap("body", apiRequestBody.getContent());
+
+            PutItemOutcome putItemOutcome = dynamoDB.getTable(TABLE_NAME).putItem(item);
+            return Map.of("statusCode", 201, "event", putItemOutcome);
+        } catch (JsonProcessingException e) {
+            throw new RuntimeException(e);
+        }
+    }
 }
