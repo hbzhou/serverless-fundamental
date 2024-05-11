a6bf0fcf7060613ef3706b407b694c0ce1f8bc34
Jeremy Zhou <jeremygilbert@126.com>
2024-05-11 22:18:46 +0800

    task07

--- task07/jsrc/main/java/com/task07/UuidGenerator.java
+++ task07/jsrc/main/java/com/task07/UuidGenerator.java
@@ -9,6 +9,7 @@ import com.amazonaws.services.s3.model.ObjectMetadata;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
 import com.syndicate.deployment.annotations.events.RuleEventSource;
 import com.syndicate.deployment.annotations.lambda.LambdaHandler;
 import com.syndicate.deployment.model.RetentionSetting;
@@ -44,7 +45,7 @@ public class UuidGenerator implements RequestHandler<Object, Map<String, Object>
                 .mapToObj(i -> UUID.randomUUID().toString())
                 .collect(Collectors.toList());
         try {
-            byte[] bytes = new Gson().toJson(Map.of("ids", ids)).getBytes(StandardCharsets.UTF_8);
+            byte[] bytes = new GsonBuilder().setPrettyPrinting().create().toJson(Map.of("ids", ids)).getBytes(StandardCharsets.UTF_8);
             ByteArrayInputStream inputStream = new ByteArrayInputStream(bytes);
             final AmazonS3 s3 = AmazonS3ClientBuilder.standard().withRegion(REGION).build();
             s3.putObject(BUCKET_NAME, filename, inputStream, new ObjectMetadata());
