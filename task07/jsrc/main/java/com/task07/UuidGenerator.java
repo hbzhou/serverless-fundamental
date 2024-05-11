31f3c3c07214ed71e0ba0c6dafae9da3ffe56fc3
Jeremy Zhou <jeremy_zhou@epam.com>
2024-05-11 16:16:13 +0800

    task07

---
+++ task07/jsrc/main/java/com/task07/UuidGenerator.java
@@ -0,0 +1,54 @@
+package com.task07;
+
+import com.amazonaws.AmazonServiceException;
+import com.amazonaws.services.lambda.runtime.Context;
+import com.amazonaws.services.lambda.runtime.RequestHandler;
+import com.amazonaws.services.s3.AmazonS3;
+import com.amazonaws.services.s3.AmazonS3ClientBuilder;
+import com.amazonaws.services.s3.model.ObjectMetadata;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.syndicate.deployment.annotations.events.RuleEventSource;
+import com.syndicate.deployment.annotations.lambda.LambdaHandler;
+import com.syndicate.deployment.model.RetentionSetting;
+
+import java.io.ByteArrayInputStream;
+import java.io.File;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.UUID;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
+@LambdaHandler(lambdaName = "uuid_generator",
+        roleName = "uuid_generator-role",
+        logsExpiration = RetentionSetting.SYNDICATE_ALIASES_SPECIFIED
+)
+@RuleEventSource(
+        targetRule = "uuid_trigger"
+)
+public class UuidGenerator implements RequestHandler<Object, Map<String, Object>> {
+    private static final String REGION = "eu-central-1";
+    private static final String BUCKET_NAME = "cmtr-8517cab4-uuid-storage-test";
+
+    public Map<String, Object> handleRequest(Object request, Context context) {
+        System.out.println("Hello from UuidGenerator lambda");
+        String filename = LocalDateTime.now().format(DateTimeFormatter.ISO_INSTANT);
+        List<String> ids = IntStream.range(1, 10)
+                .mapToObj(i -> UUID.randomUUID().toString())
+                .collect(Collectors.toList());
+        try {
+            byte[] bytes = new ObjectMapper().writeValueAsBytes(Map.of("ids", ids));
+            ByteArrayInputStream inputStream = new ByteArrayInputStream(bytes);
+            final AmazonS3 s3 = AmazonS3ClientBuilder.standard().withRegion(REGION).build();
+            s3.putObject(BUCKET_NAME, filename, inputStream, new ObjectMetadata());
+        } catch (Exception e) {
+            System.err.println(e.getMessage());
+            System.exit(1);
+        }
+        return Map.of("statusCode", 201);
+    }
+}
